7e1d34a5154986485117463b46d8eb7b46218146
Nils Loewen <nloewen@acromedia.com>
2021-06-20 21:58:55 -0700

    Decent progress for the first night.

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,94 @@
 //  ♠, ♥, ♣, ♦
+use std::env;
+use std::any::Any;
+// Todo: Make Suite a printable enum.
+// use std::fmt;
+// enum Suite { Clubs, Diamonds, Hearts, Spades, Special }
+//
+// impl fmt::Display for Suite {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         // The `f` value implements the `Write` trait, which is what the
+//         // write! macro is expecting. Note that this formatting ignores the
+//         // various flags provided to format strings.
+//         write!(f, "({})", String::from(self.type_id()))
+//     }
+// }
+
+enum State { Betting, Playing }
 
 struct Card {
-    value: usize,
     name: String,
-    suite: String,
+    value: usize,
+    suite: String
+    // suite: &'a Suite,
 }
 
 struct Player {
     name: String,
+    ordinal: usize,
     score: u8,
-    ordinal: u8,
+}
+
+struct Bet {
+    player: Player,
+    bet: u8,
+    tricks: u8
 }
 
 struct Round {
-    dealer: Player,
-    bets: (Player, u8),
+    state: State,
+    turn: usize,
+    // Todo: Make reference to Player object.
+    dealer: String,
+    // trump: Suite,
+    trump: String,
+    bets: Vec<Bet>
 }
 
 fn main() {
     print_wizard_ascii_art();
 
-    // set_players();
-    let num_rounds = 60 / num_players;
-
-    let mut deck = get_deck();
-    for card in deck {
-        println!("{} {} {}", card.value, card.name, card.suite);
+    // Setup.
+    let mut players = get_players();
+    for player in &players {
+        println!("Name: {}, Score: {}", player.name, player.score);
     }
 
-    for round in 0..num_rounds {
-        let current_round = round + 1;
+    let num_rounds = 60 / players.len();
+
+    let deck = get_deck();
+    for card in &deck {
+        println!("{:>2} {:>6} {}", card.value, card.name, card.suite);
+    }
+
+    // Rounds.
+    for round_num in 0..num_rounds {
+        let _current_round = round_num + 1;
+        let round = Round {
+            state: State::Betting,
+            turn: _current_round,
+            dealer: String::from(&players[0].name),
+            trump: String::from("Clubs"),
+            bets: vec![]
+        };
         // Betting
         // Dealer
         // Order of players
-        //
-
-
     }
 }
 
-fn set_players() -> Vec<Player> {
-    let mut players: Vec<Player> = Vec::new();
-    let names = env::args.collect();
+fn get_players() -> Vec<Player> {
+    let mut args: Vec<String> = env::args().collect();
+    // Drop the first arg as it is the command name.
+    args.drain(    0..1);
 
-    for (ordinal, name) in names.iter().enumerate() {
-      players.push(Player {
-          name,
-          ordinal,
-          score: 0
-      })
+    let mut players: Vec<Player> = Vec::new();
+    for (ordinal, name) in args.iter().enumerate() {
+        players.push(Player {
+            name: name.clone(),
+            score: 0,
+            ordinal
+        })
     }
 
     players
@@ -58,39 +98,57 @@ fn print_wizard_ascii_art() {
     println!("           _                  _\n          (_)                | |\n __      ___ ______ _ _ __ __| |\n \\ \\ /\\ / / |_  / _` | \'__/ _` |\n  \\ V  V /| |/ / (_| | | | (_| |\n   \\_/\\_/ |_/___\\__,_|_|  \\__,_|\n");
 }
 
-fn get_num_players() -> u8 {
-    let mut num_players: u8 = 0;
-    while num_players < 3 || num_players > 6 {
-        let mut input = String::new();
-
-        println!("Enter the number of players (3-6):");
-        std::io::stdin().read_line(&mut input).unwrap();
-
-        let try_num_players = input.trim().parse::<u8>();
-        num_players = match try_num_players {
-            Ok(num) => num,
-            Err(e) => {println!("not a number ({})", e); 0},
-        };
-    }
-
-    num_players
-}
-
 fn get_deck() -> Vec<Card> {
+    // Build normal 52 card deck.
     let mut deck: Vec<Card> = Vec::new();
-
-    let names = ["Jester", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace", "Wizard"];
-
-    for suite in ["Clubs", "Diamonds", "Hearts", "Spades"] {
+    let names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+    // let suites = [Suite::Clubs, Suite::Diamonds, Suite::Hearts, Suite::Spades ];
+   let suites = ["Clubs", "Diamonds", "Hearts", "Spades"];
+    for suite in suites {
         for (index, name) in names.iter().enumerate() {
             deck.push( Card {
-                value: index,
+                value: index + 2,
                 name: String::from(name.clone()),
                 suite: String::from(suite)
             });
         }
     }
 
-    // Todo: Shuffle deck
+    // Add 4 Wizards.
+    for i in 0..4 {
+        deck.push(Card {
+            name: String::from("Wizard"),
+            value: 15,
+            suite: String::from("Special")
+        });
+    }
+
+    // Add 4 Jesters.
+    for i in 0..4 {
+        deck.push(Card {
+            name: String::from("Jester"),
+            value: 0,
+            suite: String::from("Special")
+            // suite: &Suite::Special
+        });
+    }
+
+    println!("Unshuffled.");
+    for card in &deck {
+        println!("{} {} {}", card.value, card.name, card.suite);
+    }
+
+    // use rand::seq::SliceRandom;
+    // use rand::thread_rng;
+    // let mut shuffleable = deck.as_mut_slice();
+
+    // for card in shuffleable {
+    //     println!("{} {} {}", card.value, card.name, card.suite);
+    // }
+
+    // shuffleable.shuffle(&mut rng);
+    // for card in shuffleable {
+    //     println!("{} {} {}", card.value, card.name, card.suite);
+    // }
     deck
 }
