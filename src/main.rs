a7fb4e54d6ff2849db94a675c5e4234662aaafd7
Nils Loewen <nloewen@acromedia.com>
2021-06-21 19:26:10 -0700

    Suit is an Enum.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,27 @@
 //  ♠, ♥, ♣, ♦
 use std::env;
-use std::any::Any;
-// Todo: Make Suite a printable enum.
-// use std::fmt;
-// enum Suite { Clubs, Diamonds, Hearts, Spades, Special }
-//
-// impl fmt::Display for Suite {
-//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-//         // The `f` value implements the `Write` trait, which is what the
-//         // write! macro is expecting. Note that this formatting ignores the
-//         // various flags provided to format strings.
-//         write!(f, "({})", String::from(self.type_id()))
-//     }
-// }
+use std::fmt;
+
+#[derive(Clone, Copy)]
+enum Suit { Club, Diamond, Heart, Spade, Special }
+impl fmt::Display for Suit {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Suit::Club => write!(f, "♣"),
+            Suit::Diamond => write!(f, "♦"),
+            Suit::Heart => write!(f, "♥"),
+            Suit::Spade => write!(f, "♠"),
+            Suit::Special => write!(f, "S"),
+        }
+    }
+}
 
 enum State { Betting, Playing }
 
 struct Card {
     name: String,
     value: usize,
-    suite: String
-    // suite: &'a Suite,
+    suit: Suit
 }
 
 struct Player {
@@ -40,8 +41,7 @@ struct Round {
     turn: usize,
     // Todo: Make reference to Player object.
     dealer: String,
-    // trump: Suite,
-    trump: String,
+    trump: Suit,
     bets: Vec<Bet>
 }
 
@@ -58,7 +58,7 @@ fn main() {
 
     let deck = get_deck();
     for card in &deck {
-        println!("{:>2} {:>6} {}", card.value, card.name, card.suite);
+        println!("{:>2} {:>6} {}", card.value, card.name, card.suit);
     }
 
     // Rounds.
@@ -68,7 +68,7 @@ fn main() {
             state: State::Betting,
             turn: _current_round,
             dealer: String::from(&players[0].name),
-            trump: String::from("Clubs"),
+            trump: Suit::Club,
             bets: vec![]
         };
         // Betting
@@ -102,40 +102,34 @@ fn get_deck() -> Vec<Card> {
     // Build normal 52 card deck.
     let mut deck: Vec<Card> = Vec::new();
     let names = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
-    // let suites = [Suite::Clubs, Suite::Diamonds, Suite::Hearts, Suite::Spades ];
-   let suites = ["Clubs", "Diamonds", "Hearts", "Spades"];
-    for suite in suites {
+    let suits = [Suit::Club, Suit::Diamond, Suit::Heart, Suit::Spade ];
+    for suit in suits {
         for (index, name) in names.iter().enumerate() {
             deck.push( Card {
                 value: index + 2,
                 name: String::from(name.clone()),
-                suite: String::from(suite)
+                suit: suit
             });
         }
     }
 
-    // Add 4 Wizards.
+    // Add 4 Wizards and Jesters..
     for i in 0..4 {
         deck.push(Card {
             name: String::from("Wizard"),
             value: 15,
-            suite: String::from("Special")
+            suit: Suit::Special
         });
-    }
-
-    // Add 4 Jesters.
-    for i in 0..4 {
         deck.push(Card {
             name: String::from("Jester"),
             value: 0,
-            suite: String::from("Special")
-            // suite: &Suite::Special
+            suit: Suit::Special
         });
     }
 
     println!("Unshuffled.");
     for card in &deck {
-        println!("{} {} {}", card.value, card.name, card.suite);
+        println!("{} {} {}", card.value, card.name, card.suit);
     }
 
     // use rand::seq::SliceRandom;
@@ -143,12 +137,12 @@ fn get_deck() -> Vec<Card> {
     // let mut shuffleable = deck.as_mut_slice();
 
     // for card in shuffleable {
-    //     println!("{} {} {}", card.value, card.name, card.suite);
+    //     println!("{} {} {}", card.value, card.name, card.suit);
     // }
 
     // shuffleable.shuffle(&mut rng);
     // for card in shuffleable {
-    //     println!("{} {} {}", card.value, card.name, card.suite);
+    //     println!("{} {} {}", card.value, card.name, card.suit);
     // }
     deck
 }
